65598069f38848db0d3abe0266c8256747773b07
Héber <fabioalves3160@gmail.com>
2024-08-17 22:34:31 -0300

    primeiro commit.

---
+++ src/Modelos/auxiliares/Teste.java
@@ -0,0 +1,14 @@
+package Modelos.auxiliares;
+
+public class Teste {
+    public static void main(String[] args) {
+        String nomeAluno = "Heber";
+        String raAluno = "123";
+        String emailAluno = "testegmail";
+        String senhaAluno = "12345";
+
+        String dadosAluno = nomeAluno + "," + raAluno + "," + emailAluno + ","
+                + senhaAluno;
+        System.out.println(dadosAluno);
+    }
+}
